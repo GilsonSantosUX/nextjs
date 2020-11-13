1f6467f9c793652cdb3f0ddf85fc4f872a2674d0
Gilson Santos <gilsonjosert@gmail.com>
2020-11-13 21:15:56 +0000

    Ajusts GlobalStyle

--- style/GlobalStyle.js
+++ style/GlobalStyle.js
@@ -41,7 +41,6 @@ const GlobalStyle = createGlobalStyle`
     }
 
     html,body{
-        margin:0;
         padding:0;
         background: ${props => props.theme ? `var(${props.theme})` : 'var(--theme-default)' };
     }
