99a796ae5b85a4805a451d84a8ffe0cfa4f954de
Gilson Santos <gilsonjosert@gmail.com>
2023-11-05 12:41:14 -0400

    Update Header.js
    Add Clarity analitics

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,13 @@ function Header(props){
             <meta property="og:title" content="Gilson Santos UX" key="title" />
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-G5YBNBNZ3N"></script>
             <script async src="../src/analytics/analytics.js"></script>
+            <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "jlu3ebbd7z");
+            </script>
         </Head>
     )
 }
